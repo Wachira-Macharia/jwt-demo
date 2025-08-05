5223335114f7781ab0bf320e205e5a32f15130eb
Wachira-Macharia <eugenecomboni@gmail.com>
2025-08-05 12:41:02 +0300

    jwt-demo

---
+++ src/main/java/com/comboni/jwt_demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.comboni.jwt_demo.repository;
+
+import com.comboni.jwt_demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+}
